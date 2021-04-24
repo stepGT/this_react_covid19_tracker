4bf952963fe9b4cec4cca3fbd417c0f56ebadec7
stepGT <stepgt@mail.ru>
2021-04-25 06:45:46 +1000

    Added REST API api.covid19api.com

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
+const APICovid19 = "https://api.covid19api.com/summary";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(APICovid19)
+      .then((res) => res.json())
+      .then((res) => this.setState({ ...this.state, data: res.Countries }));
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.data &&
+          this.state.data.map((el) => {
+            return <li key={el.ID}>{el.Country}</li>;
+          })}
+      </ul>
+    );
+  }
 }
 
 export default App;
