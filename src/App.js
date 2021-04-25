e596854ca689b691f1c122ac3eb315a664866b3b
stepGT <stepgt@mail.ru>
2021-04-26 02:17:15 +1000

    Test Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
 import "./App.css";
 
 // https://corona.lmao.ninja/docs/
@@ -11,6 +12,7 @@ function App() {
       .then((res) => res.json())
       .then((res) => setAllData(res));
   });
+  return <Button>Button</Button>;
 }
 
 export default App;
