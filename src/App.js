3b613c949d8e821238de790b421f792d8bcbcd11
stepGT <stepgt@mail.ru>
2021-04-26 01:31:59 +1000

    Fetch data from https://disease.sh api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import { Component } from "react";
 
-const APICovid19 = "https://api.covid19api.com/summary";
+// https://corona.lmao.ninja/docs/
+const APICovid19 = "https://disease.sh/v3/covid-19/all";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+      data: {},
     };
   }
 
   componentDidMount() {
     fetch(APICovid19)
       .then((res) => res.json())
-      .then((res) => this.setState({ ...this.state, data: res.Countries }));
+      .then((res) => this.setState({ ...this.state, data: res }));
   }
 
   render() {
     return (
       <ul>
-        {this.state.data &&
-          this.state.data.map((el) => {
-            return <li key={el.ID}>{el.Country}</li>;
+        {Object.keys(this.state.data).forEach(val => {
+            console.log(val)
           })}
       </ul>
     );
