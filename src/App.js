c110964374801f9019754080fa96df49141f5c5d
stepGT <stepgt@mail.ru>
2021-04-27 03:21:22 +1000

    Added CardDeck and holderjs package

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
 import React, { useState, useEffect } from "react";
-import Button from 'react-bootstrap/Button';
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "holderjs";
 import "./App.css";
 
 // https://corona.lmao.ninja/docs/
-const APICovid19 = "https://disease.sh/v3/covid-19/all";
+const APICovid19All = "https://disease.sh/v3/covid-19/all";
 
 function App() {
-  const [allData, setAllData] = useState()
+  const [allData, setAllData] = useState();
   useEffect(() => {
-    fetch(APICovid19)
+    fetch(APICovid19All)
       .then((res) => res.json())
       .then((res) => setAllData(res));
   });
-  return <Button>Button</Button>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <CardDeck>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This card has even longer
+                  content than the first to show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
